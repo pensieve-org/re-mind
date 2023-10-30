173a3ce061224c7c2878a53ca748ce01cacfef21
James Heavey <james@heavey.net>
2023-10-30 20:47:33 +0000

    added user context

--- share-app/app/_layout.tsx
+++ share-app/app/_layout.tsx
@@ -1,10 +1,20 @@
+import { createContext, useState } from "react";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+export const UserContext = createContext(null);
+
 export default function HomeLayout() {
+  const [name, setName] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
+
   return (
-    <Slot>
-      <StatusBar style="light" />
-    </Slot>
+    <UserContext.Provider
+      value={{ name, setName, profilePicture, setProfilePicture }}
+    >
+      <Slot>
+        <StatusBar style="light" />
+      </Slot>
+    </UserContext.Provider>
   );
 }
