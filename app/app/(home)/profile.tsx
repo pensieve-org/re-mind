ec0038c98b8fa07e31d3c12c1055bb52dc34664f
James Heavey <james@heavey.net>
2023-11-22 12:50:39 +0000

    formatting

--- app/app/(home)/profile.tsx
+++ app/app/(home)/profile.tsx
@@ -163,6 +163,7 @@ export default function Profile() {
                   style={{ color: theme.PRIMARY }}
                 />
               )}
+
               <Pressable
                 onPress={handleProfilePictureUpload}
                 style={({ pressed }) => [
