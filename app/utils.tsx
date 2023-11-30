97a3edbf98d1243f7ba63566ee5a302c6198a466
James Heavey <james@heavey.net>
2023-11-30 18:33:35 +0000

    fixed image upload crash

--- app/utils.tsx
+++ app/utils.tsx
@@ -1,11 +1,15 @@
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-export const uploadImageAsync = async (uri, filepath = null) => {
-  const response = await fetch(uri);
-  const blob = await response.blob();
-
-  const storageRef = ref(getStorage(), filepath);
-  await uploadBytes(storageRef, blob);
-
-  return await getDownloadURL(storageRef);
+export const uploadImageAsync = async (uri, path) => {
+  let URL;
+  try {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    const storageRef = ref(getStorage(), path);
+    await uploadBytes(storageRef, blob);
+    URL = await getDownloadURL(storageRef);
+    return URL;
+  } catch (e) {
+    throw e;
+  }
 };
