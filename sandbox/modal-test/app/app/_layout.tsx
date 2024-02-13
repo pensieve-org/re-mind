0298648569935c5ff876e2da17b51f602f3fcc41
James Heavey <james@heavey.net>
2024-02-13 23:43:54 +0000

    fixed the fucking flickering

--- sandbox/modal-test/app/app/_layout.tsx
+++ sandbox/modal-test/app/app/_layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Slot />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
