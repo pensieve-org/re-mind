1c8079a856868cf6e6d02ad1b44879850826d7f6
James Heavey <james@heavey.net>
2024-05-30 17:42:37 +0100

    fixed version errors

--- app/babel.config.js
+++ app/babel.config.js
@@ -4,7 +4,6 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
-      require.resolve("expo-router/babel"),
       "react-native-reanimated/plugin",
     ],
   };
